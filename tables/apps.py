3e6a23e76ba8e7c00df48262b016b88141b36a81
user <kilavyk@yandex.ru>
2025-09-28 20:26:13 +0300

    fix renamed

--- tables/apps.py
+++ tables/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class UserConfig(AppConfig):
+class TablesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "Users"
+    name = "tables"
