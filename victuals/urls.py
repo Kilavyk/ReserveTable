f07fd34a9f25ff482d5b51cc065795964a312445
user <kilavyk@yandex.ru>
2025-10-10 18:44:05 +0300

    setting up the menu in the personnel panel

--- victuals/urls.py
+++ victuals/urls.py
@@ -7,4 +7,8 @@ app_name = 'victuals'
 
 urlpatterns = [
     path('menu/', views.menu_view, name='menu'),
+    path('management/', views.menu_management, name='menu_management'),
+    path('add/', views.add_menu_item, name='add_menu_item'),
+    path('edit/', views.edit_menu_item, name='edit_menu_item'),
+    path('delete/', views.delete_menu_item, name='delete_menu_item'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
